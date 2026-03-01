e1ab5ffa79f4acc7586ae60c7c70cec37bcc9ff2
Om Shimpi <omshimpi07@gmail.com>
2026-03-01 17:10:53 +0530

    Day 6 : Buy & Sell Stocks Price

--- Arrays/121_Buy_and _Sell_Stocks.java
+++ Arrays/121_Buy_and _Sell_Stocks.java
@@ -6,19 +6,20 @@ You want to maximize your profit by choosing a single day to buy one stock and
 choosing a different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction. If you cannot
 achieve any profit, return 0.
+
 Example 1:
 Input: prices = [7,1,5,3,6,4]
 Output: 5
 Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 Not 7-1 = 6, as selling price needs to be larger than buying price.
+
 Example 2:
 Input: prices = [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
-Constraints:
-1 <= prices.length <= 10^5
-0 <= prices[i] <= 10^4
+
 Difficulty: Easy
+
 Approach:
 1. Initialize two variables: `lowest_buy` to track the lowest price seen so far and `max_profit` to track the maximum profit.
 2. Iterate through the `prices` array:
