207f289c9f0b89619ae14b1b2fed8809bc253da6
Om Shimpi <omshimpi07@gmail.com>
2026-03-07 22:09:02 +0530

    Day 13 : Intersection of Two Arrays

--- Arrays/349_Intersection_of_Two_Arrays.java
+++ Arrays/349_Intersection_of_Two_Arrays.java
@@ -1,5 +1,6 @@
-/*link: https://leetcode.com/problems/intersection-of-two-arrays/description/
+/*
 349. Intersection of Two Arrays
+link: https://leetcode.com/problems/intersection-of-two-arrays/description/
 Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 Example 1:
 Input: nums1 = [1,2,2,1], nums2 = [2] Output: [2]
